055e897323ed6d8af63c1483e0ef96a7867b181e
Sean LaFlam <sean.laflam@gmail.com>
2021-02-26 23:15:42 -0500

    Queues

---
+++ Queues.js
@@ -0,0 +1,10 @@
+// --- Queues ---
+
+// Linear collection of nodes that adds (enqueues) to the tail and removes (dequeues) from the head
+
+// Think of an line at the bank or an amusement park. First come first serve (FIFO)
+
+// IMPLEMENTATION 
+// Use a LL as the underlying Data Structure. Front of queue is tail, back is head
+// Requires reference to the head and tail nodes. All other modification disallowed (i.e. cant add to middle)
+
