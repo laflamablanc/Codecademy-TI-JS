90868597e2e12d5d9ff1450396fcb2c1fc52552b
Sean LaFlam <sean.laflam@gmail.com>
2021-03-07 17:20:26 -0500

    Recursion

---
+++ Recursion.js
@@ -0,0 +1,8 @@
+// --- Recusrion ---
+
+// strategy for solving problems by defining the problem in terms of itself.
+// For example, to sum the elements of a list, we would take the first element, 
+//and add it to the sum of the remaining elements
+
+//-- Recursion in JS ---
+
