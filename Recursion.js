e0c0a7a304ba59e59ee833f4c2207daedfe8f19d
Sean LaFlam <sean.laflam@gmail.com>
2021-03-08 23:34:55 -0500

    Recursion

--- Recursion.js
+++ Recursion.js
@@ -5,4 +5,23 @@
 //and add it to the sum of the remaining elements
 
 //-- Recursion in JS ---
+// often recursion code is simpler than a while loop
 
+const iterativeFactorial = (n) => {
+    let result = 1;
+    while(n > 0) {
+      result *= n;
+      n -= 1;
+    }
+    return result;
+  }
+  
+  // Set fourFactorial
+  
+  
+  
+  module.exports = {
+    iterativeFactorial
+  };
+
+  
