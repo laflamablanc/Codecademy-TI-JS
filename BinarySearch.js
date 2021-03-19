5b7f7b175303d74386ea392bbcb6a74a33a08088
Sean LaFlam <sean.laflam@gmail.com>
2021-03-19 19:27:57 -0400

    Binary Search

---
+++ BinarySearch.js
@@ -0,0 +1,8 @@
+const binarySearch = (arr, target) => {
+    // Add left and right variables below
+    let left = 0
+    let right = arr.length
+    // Add indexToCheck calculation below
+    const indexToCheck = Math.floor((left + right)/2)
+    return indexToCheck
+  }
