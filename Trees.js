dd2c30e3e030b801131483c168ee3ef4fab12c12
Sean LaFlam <sean.laflam@gmail.com>
2021-03-19 17:05:48 -0400

    Added Print to TreeNode

--- Trees.js
+++ Trees.js
@@ -6,12 +6,34 @@ class TreeNode {
       this.children = [];
     }
   
-    addChild(child){
-      if (child.constructor.name === "TreeNode") {
-        this.children.push(child)
+    addChild(child) {
+      if (child instanceof TreeNode) {
+        this.children.push(child);
       } else {
-        const tree = new TreeNode(child)
-        this.children.push(tree)
+        this.children.push(new TreeNode(child));
       }
     }
+    
+    removeChild(childToRemove) {
+      const length = this.children.length;
+      this.children = this.children.filter(child => {
+        return childToRemove instanceof TreeNode
+        ? child !== childToRemove
+        : child.data !== childToRemove;
+      });
+  
+      if (length === this.children.length) {
+        this.children.forEach(child => child.removeChild(childToRemove));
+      }
+    }
+  
+    print(level = 0) {
+      let result = '';
+      for (let i = 0; i < level; i++) {
+        result += '-- ';
+      }
+      console.log(`${result}${this.data}`);
+      this.children.forEach(child => child.print(level + 1));
+    }
   };
+
