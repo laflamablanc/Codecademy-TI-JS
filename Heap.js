82907f5760c1bd9fa5919ad9668b132aa26a5e43
Sean LaFlam <sean.laflam@gmail.com>
2021-03-20 14:44:59 -0400

    Heap

--- Heap.js
+++
@@ -1,5 +0,0 @@
-// Heaps are another variation of the tree data structure and are adept at keeping track of 
-// the maximum or minimum value held within, referred to as max-heaps and min-heaps, respectively. 
-// Specifically, heaps are a type of binary tree, since each child node is either greater or less than 
-// its parent (depending on if it’s a max-heap or min-heap). They are efficient for accessing the root value, 
-// which will either be the max or min (again, depending on the type of heap) and inserting new values.
