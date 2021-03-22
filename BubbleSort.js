517bf8b983d6ed031bf75e1c8b5b64e7bd574807
Sean LaFlam <sean.laflam@gmail.com>
2021-03-21 23:08:51 -0400

    Swap

---
+++ BubbleSort.js
@@ -0,0 +1,23 @@
+const bubbleSort = input => {
+  let swapCount = 0
+  let swapping = true;
+  
+  while (swapping) {
+    swapping = false;
+    for (let i = 0; i < input.length - 1; i++) {
+      if (input[i] > input[i + 1]) {
+        swap(input, i, i + 1);
+        swapCount++;
+        swapping = true;
+      }
+    }
+  }
+  console.log(`Swapped ${swapCount} times`);
+  return input;
+};
+
+const swap = (arr, indexOne, indexTwo) => {
+    const temp = arr[indexTwo];
+    arr[indexTwo] = arr[indexOne];
+    arr[indexOne] = temp;
+};
