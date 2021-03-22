2ce04776c1b169aac6e9bd7a63c726663d9815f1
Sean LaFlam <sean.laflam@gmail.com>
2021-03-21 23:56:46 -0400

    Sort

--- QuickSort.js
+++ QuickSort.js
@@ -1,1 +1,12 @@
 //Quicksort is an efficient sorting algorithm that is based on the divide and conquer strategy.
+
+
+
+
+
+
+const swap = (arr, indexOne, indexTwo) => {
+    const temp = arr[indexTwo];
+    arr[indexTwo] = arr[indexOne];
+    arr[indexOne] = temp;
+}
