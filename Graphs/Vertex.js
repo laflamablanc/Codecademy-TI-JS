7e9145b2bafd17850e7f06a6f7cc7a76628554ec
Sean LaFlam <sean.laflam@gmail.com>
2021-03-22 18:24:04 -0400

    Add Edge

--- Graphs/Vertex.js
+++ Graphs/Vertex.js
@@ -1,15 +1,23 @@
 const Edge = require('./Edge.js');
 
 class Vertex {
-  
-  constructor(data){
-    this.data = data,
-    this.edges = []
+  constructor(data) {
+    this.data = data;
+    this.edges = [];
   }
-  
+
+  addEdge(vertex){
+    if (vertex instanceof Vertex) {
+      const edge = new Edge(this, vertex)
+      this.edges.push(edge)
+    } else {
+      throw 'Vertex is not valid!'
+    }
+  }
+
   print() {
     const edgeList = this.edges.map(edge =>
-        edge.weight !== null ? `${edge.end.data} (${edge.weight})` : edge.end.data) || [];
+        edge.weight !== null ? `${edge.end.data} (${edge.weight})` : edge.end.data);
 
     const output = `${this.data} --> ${edgeList.join(', ')}`;
     console.log(output);
