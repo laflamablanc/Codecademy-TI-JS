50dd68fbcc88faac1d61a9abb274158040f5d9e1
Sean LaFlam <sean.laflam@gmail.com>
2021-03-24 19:07:48 -0400

    Directed Graph

---
+++ Graphs/DirectedGraph.js
@@ -0,0 +1,47 @@
+const Edge = require('./Edge.js');
+const Vertex = require('./Vertex.js');
+
+class Graph {
+  constructor(isWeighted = false) {
+    this.vertices = [];
+    this.isWeighted = isWeighted;
+  }
+
+  addVertex(data) {
+    const newVertex = new Vertex(data);
+    this.vertices.push(newVertex);
+
+    return newVertex;
+  }
+
+  removeVertex(vertex) {
+    this.vertices = this.vertices.filter(v => v !== vertex);
+  }
+
+  addEdge(vertexOne, vertexTwo, weight) {
+    const edgeWeight = this.isWeighted ? weight : null;
+
+    if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+      vertexOne.addEdge(vertexTwo, edgeWeight);
+      vertexTwo.addEdge(vertexOne, edgeWeight);
+    } else {
+      throw new Error('Expected Vertex arguments.');
+    }
+  }
+
+  removeEdge(vertexOne, vertexTwo) {
+    if (vertexOne instanceof Vertex && vertexTwo instanceof Vertex) {
+      vertexOne.removeEdge(vertexTwo);
+      vertexTwo.removeEdge(vertexOne);
+    } else {
+      throw new Error('Expected Vertex arguments.');
+    }
+  }
+
+  print() {
+    this.vertices.forEach(vertex => vertex.print());
+  }
+}
+
+
+module.exports = Graph;
