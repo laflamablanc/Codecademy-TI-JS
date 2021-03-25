a7abd488012931f0d76b4006c9cb727bb13cd040
Sean LaFlam <sean.laflam@gmail.com>
2021-03-24 23:27:48 -0400

    DFS

--- Graphs/DepthFirstTraversal.js
+++ Graphs/DepthFirstTraversal.js
@@ -1,16 +1,16 @@
 const testGraph = require('./testGraph.js');
 
-const depthFirstTraversal = (start, visitedVertices = [start]) => {
-  console.log(start.data);
-
-  start.edges.forEach((edge) => {
-    const neighbor = edge.end;
-
-    if (!visitedVertices.includes(neighbor)) {
-      visitedVertices.push(neighbor);
-      depthFirstTraversal(neighbor, visitedVertices);
-    }
-  });
-};
+const depthFirstTraversal = (start, callback, visitedVertices = [start]) => {
+    //accepts a callback function if you want
+    callback(start)
+    start.edges.forEach((edge) => {
+      const neighbor = edge.end;
+  
+      if (!visitedVertices.includes(neighbor)) {
+        visitedVertices.push(neighbor);
+        depthFirstTraversal(neighbor, callback, visitedVertices);
+      }
+    });
+  };
 
 depthFirstTraversal(testGraph.vertices[0]);
