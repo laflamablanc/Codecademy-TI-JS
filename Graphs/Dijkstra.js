086ac02f05f85814e9b482862f431bdc64692a76
Sean LaFlam <sean.laflam@gmail.com>
2021-03-25 17:46:59 -0400

    Djikstra's

--- Graphs/Dijkstra.js
+++ Graphs/Dijkstra.js
@@ -1,13 +1,14 @@
 // Dijkstra's Algorithm uses a combo of a Breadth First Search and a Min-Heap to find the shortest
 // path from a start vertex to every other vertex in the graph
 
-
 const PriorityQueue = require('./PriorityQueue.js');
 const testGraph = require('./testGraph.js');
 
 const dijkstras = (graph, startingVertex) => {
 	const distances = {};
 	const previous = {};
+  const queue = new PriorityQueue()
+  queue.add({vertex: startingVertex, priority: 0})
 
 	graph.vertices.forEach((vertex) => {
 		distances[vertex.data] = Infinity;
@@ -16,22 +17,22 @@ const dijkstras = (graph, startingVertex) => {
 
 	distances[startingVertex.data] = 0;
 
-  const vertex = startingVertex;
+  while (!queue.isEmpty()){
 
-	vertex.edges.forEach((edge) => {
-		const alternate = edge.weight + distances[vertex.data];
-		const neighborValue = edge.end.data;
+    const {vertex} = queue.popMin();
 
-		if (alternate < distances[neighborValue]) {
-			distances[neighborValue] = alternate;
-			previous[neighborValue] = vertex;
-		}
-	})
+    vertex.edges.forEach((edge) => {
+      const alternate = edge.weight + distances[vertex.data];
+      const neighborValue = edge.end.data;
+
+      if (alternate < distances[neighborValue]) {
+        distances[neighborValue] = alternate;
+        previous[neighborValue] = vertex;
+        
+        queue.add({vertex: edge.end, priority: distances[neighborValue]})
+      }
+    })
+  }
 	return { distances, previous };
 };
-
-const results = dijkstras(testGraph, testGraph.vertices[0]);
-console.log(results);
-
-module.exports = dijkstras;
 
